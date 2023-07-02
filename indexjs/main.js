fe4735fef688b2ca0d1fa54fba1fa8f6e6d4b251
Albinpirela <119135868+Albinpirela@users.noreply.github.com>
2023-07-02 10:50:59 -0400

    Add mobile versions and table version

--- indexjs/main.js
+++ indexjs/main.js
@@ -1,6 +1,9 @@
 const menu = document.querySelector('.menu');
 const modalMenu = document.querySelector('.menu-modal');
 const close = document.querySelector('.close');
+const nostrosMenu = document.querySelector('#nosotros-menu');
+const ProductoMenu = document.querySelector('#productos-menu');
+const contactoMenu = document.querySelector('#contacto-menu');
 
 function hiddenMenu() {
   modalMenu.classList.add('active');
@@ -14,4 +17,8 @@ function toggleMenu() {
 }
 
 menu.addEventListener('click', toggleMenu);
-close.addEventListener('click', hiddenMenu)
+close.addEventListener('click', hiddenMenu);
+nostrosMenu.addEventListener('click', hiddenMenu);
+ProductoMenu.addEventListener('click', hiddenMenu);
+contactoMenu.addEventListener('click', hiddenMenu);
+
